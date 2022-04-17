920e40fff3b4c51195e6ebc3db63ac1a89af33fb
Mbarhatov <mbarhatov@mail.ru>
2022-04-17 21:47:05 +0300

    game_v2.py

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -5,18 +5,15 @@
 import numpy as np
 
 def random_predict(number:int=1)->int:
-    '''Args:
-        number(int, optional):Загаданное число.Defaults to 1.
-        Returns:
-            int: Число попыток
-    '''
+    '''Функция с минимальным попытками угадывания чисел '''
 
-    count = 0
+    count = 0 # счетчик попыток
+    '''В переменных low и high границы угадываемого числа'''
     low = 1
     high = 101
-    predict_number = high // 2
+    predict_number = high // 2 # сужаем границы
     
-    while number != predict_number:
+    while number != predict_number: # пока эти числа не будут равны
         count += 1
         
         if number > predict_number:
@@ -25,9 +22,9 @@ def random_predict(number:int=1)->int:
         elif number < predict_number:
             high = predict_number - 1
             
-        predict_number = (low+high) // 2
+        predict_number = (low+high) // 2 # проверяем средний элимент
         
-    return(count)
+    return(count) # выход из цыкла, если угадали
 
 
 def score_game(random_predict) -> int:
